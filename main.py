5791273ec90215f62de935027b0898acb2a9505a
Marta Sajdak <crappylime@op.pl>
2017-04-04 10:43:34 +0200

    Display flashes

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from mod_events.controllers import mod_events
 from database import Database
+import os
 
 app = Flask(__name__)
 app.register_blueprint(mod_events)
+app.secret_key = os.urandom(24)
 
 Database.create_db('database.sql')
 

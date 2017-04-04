ca78d49d7163579c214d0ebb6d8d2688c5fe7c42
Marta Sajdak <crappylime@op.pl>
2017-04-04 10:14:35 +0200

    Define getter for all events

--- mod_events/models.py
+++ mod_events/models.py
@@ -9,3 +9,17 @@ class Event:
         self.date = date
         self.description = description
         self.id = id
+
+    @classmethod
+    def get_all(cls):
+        """ Retrieves all events from database and returns them as list.
+        Returns:
+            list(Event): list of all events
+        """
+        events_list = []
+        query = "SELECT * FROM `events` ORDER BY date DESC;"
+        data = Database.execute_query(query, ())
+
+        for row in data:
+            events_list.append(cls(row[1], row[2], row[3], row[0]))
+        return events_list

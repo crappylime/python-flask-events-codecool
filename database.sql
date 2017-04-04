f12fce21490062baac72078aa58675d053ae44e9
Marta Sajdak <crappylime@op.pl>
2017-04-04 09:52:34 +0200

    Create table events

--- database.sql
+++ database.sql
@@ -2,3 +2,9 @@
 --   CREATE TABLE users (id INT PRIMARY KEY NOT NULL, full_name CHAR(50) NOT NULL);
 --
 -- Put statments below
+CREATE TABLE IF NOT EXISTS `events`(
+	`id`	INTEGER PRIMARY KEY AUTOINCREMENT,
+	`name`	TEXT,
+	`date`	DATE,
+	`description`	TEXT
+);

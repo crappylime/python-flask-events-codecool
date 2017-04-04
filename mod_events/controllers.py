ce287c298d8c7fd71cf9f0296f59ce7c56ec7f7b
Marta Sajdak <crappylime@op.pl>
2017-04-04 10:41:39 +0200

    Add /create route

--- mod_events/controllers.py
+++ mod_events/controllers.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, url_for, redirect, flash
 from mod_events.models import Event
 
 mod_events = Blueprint('mod_events', __name__)
@@ -10,3 +10,24 @@ def index():
     """ Shows list of events stored in the database.
     """
     return render_template('index.html', events_list=Event.get_all())
+
+
+@mod_events.route("/create", methods=['GET', 'POST'])
+def create():
+    """ Creates new event
+    If the method was GET it should show new event form.
+    If the method was POST it should create and save new event.
+    """
+    if request.method == 'POST':
+        if not request.form['name']:
+            flash("Event's name is required")
+        elif not request.form['date']:
+            flash("Event's date is required")
+        elif not request.form['description']:
+            flash("Event's description is required")
+        else:
+            event = Event(name=request.form['name'], date=request.form['date'], description=request.form['description'])
+            event.save()
+            return redirect(url_for('mod_events.index'))
+
+    return render_template('form.html')

0cb7847f248d2ac74c798d03537edc2bec258923
Marta Sajdak <crappylime@op.pl>
2017-04-04 10:41:02 +0200

    Create method that saves event to database

--- mod_events/models.py
+++ mod_events/models.py
@@ -23,3 +23,12 @@ class Event:
         for row in data:
             events_list.append(cls(row[1], row[2], row[3], row[0]))
         return events_list
+
+    def save(self):
+        """ Saves/updates event in database """
+        if self.id:
+            query = "UPDATE `events` SET `name` = ?, `date` = ?, `description` = ? WHERE `id` = ?;"
+            Database.execute_query(query, (self.name, self.date, self.description, self.id))
+        else:
+            query = 'INSERT INTO events (`name`, `date`, `description`) VALUES (?, ?, ?);'
+            Database.execute_query(query, (self.name, self.date, self.description))

e413bdb3813fedbeb93cd227098c86e707c8069b
Marta Sajdak <crappylime@op.pl>
2017-04-04 10:15:22 +0200

    Define route for showing events

--- mod_events/controllers.py
+++ mod_events/controllers.py
@@ -1,11 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 from mod_events.models import Event
 
 mod_events = Blueprint('mod_events', __name__)
 
 
 @mod_events.route("/")
+@mod_events.route("/events")
 def index():
     """ Shows list of events stored in the database.
     """
-    return 'Hello'
+    return render_template('index.html', events_list=Event.get_all())

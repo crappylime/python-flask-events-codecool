34f1c38740358620ae18c93ccd8d948d703f7f75
Marta Sajdak <crappylime@gmail.com>
2017-07-24 20:12:52 +0200

    Adjust getting connection for heroku needs

--- database.py
+++ database.py
@@ -1,8 +1,33 @@
+import os
+import urllib.parse as urlparse
+
 import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
 
 class Database:
     """Class representing database."""
+
+    @classmethod
+    def get_connection(cls):
+        urlparse.uses_netloc.append("postgres")
+        url = urlparse.urlparse(os.environ['DATABASE_URL'])
+        dbname = url.path[1:]
+        user = url.username
+        password = url.password
+        host = url.hostname
+        port = url.port
+
+        conn = psycopg2.connect(
+            dbname=dbname,
+            user=user,
+            password=password,
+            host=host,
+            port=port
+        )
+        conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+        return conn
+
 
     @classmethod
     def create_db(cls, filename):
@@ -11,7 +36,7 @@ class Database:
         with open(filename, 'r') as f:
             sqlfile = f.read()
 
-        conn = psycopg2.connect("dbname='test'")
+        conn = Database.get_connection()
         with conn:
             cur = conn.cursor()
             cur.execute(sqlfile)
@@ -19,7 +44,7 @@ class Database:
     @classmethod
     def execute_query(cls, query, args):
         """Execute query and return data"""
-        conn = psycopg2.connect("dbname='test'")
+        conn = Database.get_connection()
         cur = conn.cursor()
         cur.execute(query, args)
         data = cur.fetchall()

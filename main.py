7ce4eefe5fe4cc981afc03b3f9cf74ef07ccd387
Marta Sajdak <crappylime@gmail.com>
2017-07-24 15:47:40 +0200

    Redirect from the main page to the events list

--- main.py
+++ main.py
@@ -11,5 +11,13 @@ app.secret_key = os.urandom(24)
 
 Database.create_db('/home/lama/Code/Python/python-flask-events-codecool/database.sql')
 
+
+@app.route("/")
+def index():
+    """ Redirects to list of events stored in the database.
+    """
+    return redirect(url_for('mod_events.index'))
+
+
 if __name__ == "__main__":
     app.run()
